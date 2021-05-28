2dd3695763845d92d790f055fabed2827dffe360
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-05-29 00:59:01 +0530

    Added Movements

--- Obstacle Course/Assets/Scripts/Mover.cs
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +13,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue);
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, 0, zValue);
     }
 }
