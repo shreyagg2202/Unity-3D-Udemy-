68f939346fc8f5dac55e58cabeac3b7e31822798
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-05-29 01:04:35 +0530

    Made it framerate Independent

--- Obstacle Course/Assets/Scripts/Mover.cs
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +14,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
         transform.Translate(xValue, 0, zValue);
     }
 }
