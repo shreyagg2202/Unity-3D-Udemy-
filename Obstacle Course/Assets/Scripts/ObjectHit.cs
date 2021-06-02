890a8c267e65d2d605970fb6af24fe54c4fa00b9
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-02 16:06:12 +0530

    Created Collisions

---
+++ Obstacle Course/Assets/Scripts/ObjectHit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Bumped into the wall!");
+    }
+}
