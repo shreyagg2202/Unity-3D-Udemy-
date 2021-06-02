92d8517325b7a5ab014ad464a21e1c93044e9a4f
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-02 16:38:17 +0530

    Updated Score mechanics

--- Obstacle Course/Assets/Scripts/ObjectHit.cs
+++ Obstacle Course/Assets/Scripts/ObjectHit.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision other)
     {
-        GetComponent<MeshRenderer>().material.color = Color.red;
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            gameObject.tag = "Hit";
+        }
     }
 }
--- Obstacle Course/Assets/Scripts/Scorer.cs
+++ Obstacle Course/Assets/Scripts/Scorer.cs
@@ -6,9 +6,12 @@ public class Scorer : MonoBehaviour
 {
     int hits = 0;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision other)
     {
-        hits++;
-        Debug.Log("You've bumbed into a thing this many times: " + hits);
+        if (other.gameObject.tag != "Hit")
+        {
+            hits++;
+            Debug.Log("You've bumbed into a thing this many times: " + hits);
+        }
     }
 }
