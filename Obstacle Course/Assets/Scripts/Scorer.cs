0c4ff44cc2b6c80e1ed4f1858a3e7872d78eb433
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-02 16:15:57 +0530

    Added Score

--- Obstacle Course/Assets/Scripts/Scorer.cs
+++ Obstacle Course/Assets/Scripts/Scorer.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectHit : MonoBehaviour
+public class Scorer : MonoBehaviour
 {
+    int hits = 0;
+
     private void OnCollisionEnter(Collision collision)
     {
-        GetComponent<MeshRenderer>().material.color = Color.red;
+        hits++;
+        Debug.Log("You've bumbed into a thing this many times: " + hits);
     }
 }
