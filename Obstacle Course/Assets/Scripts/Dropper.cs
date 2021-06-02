88c7bc9caa5f3f0cc0fc07cea089096ac53817d2
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-02 16:30:07 +0530

    Added Dropping Objects

---
+++ Obstacle Course/Assets/Scripts/Dropper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    MeshRenderer renderer;
+    Rigidbody myRigidBody;
+    float timeElapsed;
+    [SerializeField] float timeToWait = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        myRigidBody = GetComponent<Rigidbody>();
+
+        renderer.enabled = false;
+        myRigidBody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed = Time.time;
+        if (timeElapsed > timeToWait)
+        {
+            renderer.enabled = true;
+            myRigidBody.useGravity = true;
+        }
+    }
+}
