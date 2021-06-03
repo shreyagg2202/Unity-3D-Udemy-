abd7363f778494c0a7caa4bc4119f0e1f1cdacd3
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-03 15:50:54 +0530

    Added Obstacle and Audio

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -43,6 +43,8 @@ public class Movement : MonoBehaviour
 
     public void ApplyRotation(float rotationThisFrame)
     {
+        rb.freezeRotation = true;    //freezing rotation so we can manually rotate
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;   //unfreezing rotation so the physics system can work
     }
 }
