93082d45e588b4006874eb144bd3918ec4d14b0c
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-03 16:16:13 +0530

    Added Restart Level

---
+++ Project Boost/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is frinedly");
+                break;
+            case "Finish":
+                Debug.Log("Finished game!!");
+                break;
+            case "Fuel":
+                Debug.Log("you picked up fuel");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
