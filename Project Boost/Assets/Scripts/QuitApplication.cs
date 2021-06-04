45230a4b0485ad10f2fe43769447983601d69352
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-05 03:36:35 +0530

    Added Escape button to quit

---
+++ Project Boost/Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Escape is pushed!");
+            Application.Quit();
+        }
+    }
+}
