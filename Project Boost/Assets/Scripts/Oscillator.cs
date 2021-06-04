767ab9b3712e0712718a988b06c31ebca06da5c2
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-05 03:10:17 +0530

    Working on Moving Obstacle

---
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0,1)] float movementFactor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+        Debug.Log(startingPosition);
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
