b439a8c704602e161c3b25882fa723cc5ab8c9ab
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-05 03:25:49 +0530

    Added Moving Obstacle

--- Project Boost/Assets/Scripts/Oscillator.cs
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -6,7 +6,8 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 2f;
     
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,14 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame 
     void Update()
     {
+        if (period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period;  //continually growing over time
+
+        const float tau = Mathf.PI * 2; //constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); //going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f;    //recalculated to go from 0 to 1 so its cleaner
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
