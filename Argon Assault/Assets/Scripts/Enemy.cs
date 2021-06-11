f9f3b81c483a5a01d8be989cf05f2fb4fd465444
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-11 16:33:34 +0530

    Added enemy Explosions

--- Argon Assault/Assets/Scripts/Enemy.cs
+++ Argon Assault/Assets/Scripts/Enemy.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+
     private void OnParticleCollision(GameObject other)
     {
-        Debug.Log("destroyed");
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
---
+++ Argon Assault/Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    [SerializeField] float timeTillDestroy = 3f;
+    void Start()
+    {
+        Destroy(gameObject, timeTillDestroy);
+    }
+}
