543c7f191538d380c507cf8389a9a0d03c86eda1
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-11 22:31:26 +0530

    Added Score UI and functionality

--- Argon Assault/Assets/Scripts/ScoreBoard.cs
+++ Argon Assault/Assets/Scripts/ScoreBoard.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TMP_Text scoreText;
+
+    void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "Start";
+    }
 
     public void IncreaseScore(int amountToIncrese)
     {
         score += amountToIncrese;
-        Debug.Log($"Score is now {score}");
+        scoreText.text = score.ToString();
+
     }
 }
