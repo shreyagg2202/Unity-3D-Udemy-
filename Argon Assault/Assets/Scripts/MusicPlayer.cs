6aac29c3e38e8d59feffdab05faa7195307566c6
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-16 13:40:23 +0530

    Added Background Music

---
+++ Argon Assault/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        int numMusiclayers = FindObjectsOfType<MusicPlayer>().Length;
+        if (numMusiclayers > 1) 
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
