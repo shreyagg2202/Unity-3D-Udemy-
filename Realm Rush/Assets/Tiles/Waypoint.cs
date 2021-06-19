212ae8058bc55e9c5a1204cbd2a2620494271d45
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-19 16:51:49 +0530

    Mouse Inputs Added

--- Realm Rush/Assets/Tiles/Waypoint.cs
+++ Realm Rush/Assets/Tiles/Waypoint.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] bool isPlacable;
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        
+        if (isPlacable)
+        {
+            Debug.Log(transform.name);
+        }
     }
 }
