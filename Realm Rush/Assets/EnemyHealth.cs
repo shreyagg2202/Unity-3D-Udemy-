6bcb89d4667419f0f12ef947e339893e688fb3d1
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-19 17:21:36 +0530

    Enemy Health Added

---
+++ Realm Rush/Assets/EnemyHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHP = 5;
+    [SerializeField] int currentHP = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        currentHP--;
+
+        if (currentHP <= 0) 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
