d246739217fcd7098adcaa4e742f2861fd583872
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-19 16:34:19 +0530

    Fixed Enemy Facing

--- Realm Rush/Assets/Enemy/EnemyMover.cs
+++ Realm Rush/Assets/Enemy/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0,5f)] float speed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +21,11 @@ public class EnemyMover : MonoBehaviour
             Vector3 endPosition = waypoint.transform.position;
             float travelPercent = 0f;
 
+            transform.LookAt(endPosition);
+
             while (travelPercent < 1f)
             {
-                travelPercent += Time.deltaTime;
+                travelPercent += Time.deltaTime * speed;
                 transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
                 yield return new WaitForEndOfFrame();
             }
