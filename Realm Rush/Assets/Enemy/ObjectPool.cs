06179548f60798266c96996b54ed708b60f10f9c
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-20 16:40:35 +0530

    Enemy Spawning Added

---
+++ Realm Rush/Assets/Enemy/ObjectPool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] float spawnTimer = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            Instantiate(enemyPrefab, transform);
+            yield return new WaitForSeconds(spawnTimer);
+        }
+    }
+}
