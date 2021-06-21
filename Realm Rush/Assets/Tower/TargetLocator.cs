9c18e242519cc00a0a82d9990f4fd7e2a05b995c
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-22 02:38:48 +0530

    Towers now target closest enemy and have a certain range

--- Realm Rush/Assets/Tower/TargetLocator.cs
+++ Realm Rush/Assets/Tower/TargetLocator.cs
@@ -1,24 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectileParticles;
+    [SerializeField] float range = 15f;
     Transform target;
 
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
+    }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
     }
 
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(transform.position, target.position);
         weapon.LookAt(target);
+        if (targetDistance < range)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+
+    void Attack(bool isActive)
+    {
+        var emmisionModule = projectileParticles.emission;
+        emmisionModule.enabled = isActive;
     }
 }
