75ddf85a54e2d5ba3ddbd7bbfc1d1aa4949c5c76
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-06-25 22:55:09 +0530

    Grid Created

--- Realm Rush (Improved)/Assets/Pathfinding/GridManager.cs
+++ Realm Rush (Improved)/Assets/Pathfinding/GridManager.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Vector2Int gridSize;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+
+    void Awake()
     {
-        
+        CreateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateGrid()
     {
-        
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                Vector2Int coordinates = new Vector2Int(x, y);
+                grid.Add(coordinates, new Node(coordinates, true));
+                Debug.Log(grid[coordinates].coordinates + "=" + grid[coordinates].isWalkable);
+            }
+        }
     }
 }
--- Realm Rush (Improved)/Assets/Pathfinding/Node.cs
+++ Realm Rush (Improved)/Assets/Pathfinding/Node.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Node
 {
     public Vector2Int coordinates;
