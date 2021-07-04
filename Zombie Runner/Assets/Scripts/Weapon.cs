6cb20ce0cd15ea32c649899f6a867120eac8e8c6
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-07-04 16:30:03 +0530

    Enemy Death condition Added

--- Zombie Runner/Assets/Scripts/Weapon.cs
+++ Zombie Runner/Assets/Scripts/Weapon.cs
@@ -7,6 +7,7 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
+    [SerializeField] float damage = 30f;
 
     void Update()
     {
@@ -19,7 +20,17 @@ public class Weapon : MonoBehaviour
     private void Shoot()
     {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        Debug.Log("I hit " + hit.transform.name);
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit " + hit.transform.name);
+            //TODO: add some hit effect for visual players
+            EnemyHealth Target = hit.transform.GetComponent<EnemyHealth>();
+            if (Target==null) return;
+            Target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
     }
 }
