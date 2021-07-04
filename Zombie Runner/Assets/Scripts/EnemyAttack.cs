db95bc7e224444a29cb0ef0f332053c0ad4a569c
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-07-05 03:06:02 +0530

    Created PlayerHealth

--- Zombie Runner/Assets/Scripts/EnemyAttack.cs
+++ Zombie Runner/Assets/Scripts/EnemyAttack.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    PlayerHealth target;
     [SerializeField] float damage = 40f;
 
     void Start()
     {
-        
+        target = FindObjectOfType<PlayerHealth>();
     }
 
     public void AttackHitEvent()
     {
         if (target == null) return;
-        Debug.Log("Bang Bang!");
+        target.TakeDamage(damage);
     }
 }
