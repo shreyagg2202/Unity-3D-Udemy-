bc14f80b9cd8bae2e5dd8d8a96047eb03bfb842c
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-07-04 15:38:46 +0530

    Added Enemy Gizmos

--- Zombie Runner/Assets/Scripts/EnemyAI.cs
+++ Zombie Runner/Assets/Scripts/EnemyAI.cs
@@ -25,4 +25,10 @@ public class EnemyAI : MonoBehaviour
             navMeshAgent.SetDestination(target.position);
         }
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
 }
