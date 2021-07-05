37c2e5077cad135c31f325797374b88fbf5097bb
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-07-05 16:19:14 +0530

    Added Game Over UI

---
+++ Zombie Runner/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void ReloadGame()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
