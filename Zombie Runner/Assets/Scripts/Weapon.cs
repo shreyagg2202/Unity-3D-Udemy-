8d6a4af980738fb077c9150702ac0d7c2c82f722
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-07-08 02:23:13 +0530

    Added Ammo

--- Zombie Runner/Assets/Scripts/Weapon.cs
+++ Zombie Runner/Assets/Scripts/Weapon.cs
@@ -10,6 +10,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] float damage = 30f;
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] GameObject hitEffect;
+    [SerializeField] Ammo ammoSlot;
 
     void Update()
     {
@@ -21,8 +22,12 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
-        PlayMuzzleFlash();
-        ProcessRayCast();
+        if (ammoSlot.GetCurrentAmmo() > 0)
+        {
+            PlayMuzzleFlash();
+            ProcessRayCast();
+            ammoSlot.ReduceCurrentAmmo();
+        }
     }
 
     private void PlayMuzzleFlash()
