25c739a4eb96068b925a9afcf7d6ddfbc49e33f2
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-07-13 16:42:02 +0530

    Added Flashlight mechanics

---
+++ Zombie Runner/Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimunAngle = 40f;
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+    private void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle <= minimunAngle) 
+        { 
+            return; 
+        }
+
+        else
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+}
