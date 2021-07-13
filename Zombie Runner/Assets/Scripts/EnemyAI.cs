3d4af85360be3bfc69685378ccfaf5129dfe3cbc
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-07-14 01:47:35 +0530

    Added Enemies and Ammo Display

--- Zombie Runner/Assets/Scripts/EnemyAI.cs
+++ Zombie Runner/Assets/Scripts/EnemyAI.cs
@@ -6,7 +6,6 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    [SerializeField] Transform target;
     [SerializeField] float chaseRange = 5f;
     [SerializeField] float turnSpeed = 5f;
 
@@ -14,11 +13,13 @@ public class EnemyAI : MonoBehaviour
     float distanceToTarget = Mathf.Infinity;
     bool isProvoked = false;
     EnemyHealth health;
+    Transform target;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         health = GetComponent<EnemyHealth>();
+        target = FindObjectOfType<PlayerHealth>().transform;
     }
 
     void Update()
