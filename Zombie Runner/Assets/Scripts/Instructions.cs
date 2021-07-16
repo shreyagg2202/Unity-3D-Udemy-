b5062a5cf476c25108a054f05b227da226be804e
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-07-16 18:15:13 +0530

    Added instructions screen

---
+++ Zombie Runner/Assets/Scripts/Instructions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instructions : MonoBehaviour
+{
+    [SerializeField] Canvas instructionsCanvas;
+    [SerializeField] Canvas ammoCanvas;
+    [SerializeField] Canvas healthCanvas;
+
+    bool gameStarted = false;
+
+    private void Start()
+    {
+        Time.timeScale = 0;
+        instructionsCanvas.enabled = true;
+        ammoCanvas.enabled = false;
+        healthCanvas.enabled = false;
+    }
+
+    private void Update()
+    {
+        if ((Input.anyKeyDown || Input.GetMouseButtonDown(0)) && gameStarted  == false)
+        {
+            Debug.Log("Game has started");
+            Time.timeScale = 1;
+            gameStarted = true;
+            instructionsCanvas.enabled = false;
+            ammoCanvas.enabled = true;
+            healthCanvas.enabled = true;
+        }
+    }
+}
--- Zombie Runner/Assets/Scripts/PlayerHealth.cs
+++ Zombie Runner/Assets/Scripts/PlayerHealth.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float hitPoints = 100f;
+    [SerializeField] TextMeshProUGUI healthText;
+
+    private void Update()
+    {
+        healthText.text = hitPoints.ToString();
+    }
 
     public void TakeDamage(float damage)
     {
