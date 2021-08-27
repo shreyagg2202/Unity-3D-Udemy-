5b60248a72532d9f0c5b7ae78064b69a302cbe78
Shrey Aggarwal <shreyagg2202@gmail.com>
2021-08-27 17:45:54 +0530

    PlayTesting

---
+++ Zombie Runner/Assets/Scripts/Finish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    [SerializeField] Canvas gameWonCanvas;
+
+    private void Start()
+    {
+        gameWonCanvas.enabled = false;
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        gameWonCanvas.enabled = true;
+        Time.timeScale = 0;
+        FindObjectOfType<WeaponSwitcher>().enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
